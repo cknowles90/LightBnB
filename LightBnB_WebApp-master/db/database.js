395bffde5ba86eece0c5838848235f2d7dc1257a
Charles Denahy-Knowles <cjknowles90@gmail.com>
2023-10-19 18:31:34 -0700

    Refactored addProperty with SQL Query.

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -192,7 +192,36 @@ const getAllProperties = (options, limit = 10) => {
  */
 const addProperty = function (property) {
 
+  const queryString = `
+  INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code,  country, parking_spaces, number_of_bathrooms, number_of_bathrooms)
+  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)
+  RETURNING *;
+  `;
+  const values = [
+    property.owner_id, 
+    property.title, 
+    property.description, 
+    property.thumbnail_photo_url, 
+    property.cover_photo_url, 
+    property.cost_per_night, 
+    property.street, 
+    property.city, 
+    property.province, 
+    property.post_code,  
+    property.country, 
+    property.parking_spaces, 
+    property.number_of_bathrooms, 
+    property.number_of_bathrooms
+  ];
   
+  return pool.query (queryString, values)
+  .then((result) => {
+    // console.log(result.rows);
+    return result.rows;
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 };
 
 module.exports = {
